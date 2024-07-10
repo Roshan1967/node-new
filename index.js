4670051afa877efa846a507c7d5d222a2e5580b9
Roshan Kumar <153509225+Roshan1967@users.noreply.github.com>
2024-07-10 14:25:30 +0530

    Add files via upload
    for the file editor assignment.

--- index.js
+++ index.js
@@ -1,59 +1,128 @@
-// index.js
+const fs = require('fs');
 
-// Import the crypto module
-const crypto = require('crypto');
+// Get command-line arguments
+const operation = process.argv[2];
+const file = process.argv[3];
+const content = process.argv[4];
 
-// Retrieve the command line arguments
-const args = process.argv.slice(2);
-
-// Validate input
-if (args.length < 1) {
-  console.log("Please provide an operation and numbers.");
-  process.exit(1);
+// Perform operations based on the command
+switch (operation) {
+  case 'read':
+    readFile(file);
+    break;
+  case 'delete':
+    deleteFile(file);
+    break;
+  case 'create':
+    createFile(file);
+    break;
+  case 'append':
+    appendToFile(file, content);
+    break;
+  case 'rename':
+    renameFile(file, content);
+    break;
+  case 'list':
+    listFiles(file);
+    break;
+  default:
+    console.log(`Invalid operation '${operation}'`);
 }
 
-// Get the operation from the first argument
-const operation = args[0];
+// Functions for each operation
 
-// Function to handle mathematical operations
-const performCalculation = (operation, numbers) => {
-  switch (operation) {
-    case 'add':
-      return numbers.reduce((acc, num) => acc + num, 0);
-    case 'sub':
-      return numbers.reduce((acc, num) => acc - num);
-    case 'mult':
-      return numbers.reduce((acc, num) => acc * num, 1);
-    case 'divide':
-      return numbers.reduce((acc, num) => acc / num);
-    case 'sin':
-      return Math.sin(numbers[0]);
-    case 'cos':
-      return Math.cos(numbers[0]);
-    case 'tan':
-      return Math.tan(numbers[0]);
-    case 'random':
-      const length = numbers[0];
-      if (length === undefined) {
-        console.log("Provide length for random number generation.");
-        process.exit(1);
-      }
-      return crypto.randomBytes(length).toString('binary');
-    default:
-      console.log("Invalid operation");
-      process.exit(1);
+function readFile(filename) {
+  if (!filename) {
+    console.log('Please provide a file to read.');
+    return;
   }
-};
-
-// Extract numbers from arguments and convert to floats
-const numbers = args.slice(1).map(Number);
-
-// Validate the numbers
-if (operation !== 'random' && numbers.some(isNaN)) {
-  console.log("Please provide valid numbers.");
-  process.exit(1);
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file '${filename}':`, err);
+    } else {
+      console.log(data);
+    }
+  });
 }
 
-// Perform the calculation and output the result
-const result = performCalculation(operation, numbers);
-console.log(`Result: ${result}`);
+function deleteFile(filename) {
+  if (!filename) {
+    console.log('Please provide a file to delete.');
+    return;
+  }
+  fs.access(filename, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error(`File '${filename}' does not exist.`);
+    } else {
+      fs.unlink(filename, (err) => {
+        if (err) {
+          console.error(`Error deleting file '${filename}':`, err);
+        } else {
+          console.log(`File '${filename}' deleted`);
+        }
+      });
+    }
+  });
+}
+
+function createFile(filename) {
+  if (!filename) {
+    console.log('Please provide a file to create.');
+    return;
+  }
+  fs.writeFile(filename, '', (err) => {
+    if (err) {
+      console.error(`Error creating file '${filename}':`, err);
+    } else {
+      console.log(`File '${filename}' created`);
+    }
+  });
+}
+
+function appendToFile(filename, content) {
+  if (!filename || !content) {
+    console.log('Please provide content and file to append.');
+    return;
+  }
+  fs.appendFile(filename, content + '\n', (err) => {
+    if (err) {
+      console.error(`Error appending content to file '${filename}':`, err);
+    } else {
+      console.log(`Content appended to the file '${filename}'`);
+    }
+  });
+}
+
+function renameFile(oldName, newName) {
+  if (!oldName || !newName) {
+    console.log('Please provide the current file name and the new file name.');
+    return;
+  }
+  fs.rename(oldName, newName, (err) => {
+    if (err) {
+      console.error(`Error renaming file '${oldName}' to '${newName}':`, err);
+    } else {
+      console.log(`File '${oldName}' renamed to '${newName}'`);
+    }
+  });
+}
+
+function listFiles(directory) {
+  if (!directory) {
+    console.log('Please provide a directory to list.');
+    return;
+  }
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.error(`Error listing directory '${directory}':`, err);
+    } else {
+      files.forEach(file => {
+        console.log(file);
+      });
+    }
+  });
+}
+
+
+
+ 
